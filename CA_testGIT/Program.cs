851db211e458fe29395d5f458915e2bd0e6eab6e
OS0659 <OS0659@9060002729-N>
2018-08-31 18:56:23 +0700

    Add project files.

---
+++ CA_testGIT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CA_testGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test lah..");
+            Console.ReadKey();
+        }
+    }
+}
